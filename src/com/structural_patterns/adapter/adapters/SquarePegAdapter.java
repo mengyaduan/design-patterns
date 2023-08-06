78eb5308a09e88c7b6b014f55e6b8146cc284e8e
yameng.dym <yameng.dym@autonavi.com>
2023-08-06 16:16:45 +0800

    add adapter

---
+++ src/com/structural_patterns/adapter/adapters/SquarePegAdapter.java
@@ -0,0 +1,21 @@
+package com.structural_patterns.adapter.adapters;
+
+import com.structural_patterns.adapter.round.RoundPeg;
+import com.structural_patterns.adapter.square.SquarePeg;
+
+public class SquarePegAdapter extends RoundPeg {
+
+    private SquarePeg squarePeg;
+
+    public SquarePegAdapter(SquarePeg peg) {
+        this.squarePeg = peg;
+    }
+
+    @Override
+    public double getRadius() {
+        double result;
+        // Calculate a minimum circle radius, which can fit this peg.
+        result = (Math.sqrt(Math.pow((squarePeg.getWidth() / 2), 2) * 2));
+        return result;
+    }
+}
